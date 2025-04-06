a086a90c0e479a8c08f11f766627cf44eb7b0a6d
Ronasunil <ronasunilcoc@gmail.com>
2025-04-06 16:58:37 +0530

    added buildspec for code build and ebexetensions

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./config.env" });
 
 const app = express();
 
-app.listen(2000, () => {
+console.log(process.env.PORT);
+
+app.listen(process.env.PORT, () => {
   console.log("Server listening on port 2000");
 });
 
 app.use("/", (req, res) => {
   res.status(200).json({ message: "Hellow" });
+});
+
+app.use("/helath", (req, res) => {
+  res.status(200).json({ message: "Hello healthy" });
 });
